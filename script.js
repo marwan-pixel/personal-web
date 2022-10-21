633d6b1420262486dfc878eb3b12347e41d32797
marwan-pixel <70478867+marwan-pixel@users.noreply.github.com>
2022-10-21 19:47:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -83,52 +83,11 @@ function b1() {
               Saya adalah mahasiswa jurusan teknik informatika dari
               Universitas Binaniaga Indonesia. Saya sangat tertarik dengan
               programming dan desain, khususnya desain web. Tak hanya itu,
-              saya juga senang memplajari berbagai bahasa, baik itu bahasa
+              saya juga senang mempelajari berbagai bahasa, baik itu bahasa
               pemrograman serta bahasa asing.
             </h4>
           </div>
-          <h4 class="bio">Bio</h4>
-          <div class="tahun">
-            <div class="tahun-detail">
-              <div class="circle"></div>
-              <h4>2002</h4>
-            </div>
-            <h4>Lahir di Bogor, Jawa Barat</h4>
-          </div>
-
-          <div class="tahun">
-            <div class="tahun-detail">
-              <div class="circle"></div>
-              <h4>2008</h4>
-            </div>
-            <h4>Mengenyam pendidikan SD di SDN Pengadilan 5 Bogor</h4>
-          </div>
-
-          <div class="tahun">
-            <div class="tahun-detail">
-              <div class="circle"></div>
-              <h4>2014</h4>
-            </div>
-            <h4>Melanjutkan pendidikan SMP di SMP PGRI 3 Bogor</h4>
-          </div>
-
-          <div class="tahun">
-            <div class="tahun-detail">
-              <div class="circle"></div>
-              <h4>2017</h4>
-            </div>
-            <h4>Melanjutkan pendidikan SMK di SMKN 2 Bogor</h4>
-          </div>
-
-          <div class="tahun">
-            <div class="tahun-detail">
-              <div class="circle"></div>
-              <h4>2020</h4>
-            </div>
-            <h4>
-              Melanjutkan pendidikan kuliah di Universitas Binaniaga Indonesia
-            </h4>
-          </div>
+          
           </div>
        `;
 }
@@ -146,12 +105,19 @@ function b1toF1() {
               Saya adalah mahasiswa jurusan teknik informatika dari
               Universitas Binaniaga Indonesia. Saya sangat tertarik dengan
               programming dan desain, khususnya desain web. Tak hanya itu,
-              saya juga senang memplajari berbagai bahasa, baik itu bahasa
+              saya juga senang mempelajari berbagai bahasa, baik itu bahasa
               pemrograman serta bahasa asing.
             </h4>
           </div>
-          <div id='bio'>
-          <h4 class="bio">Bio</h4>
+          
+        </div>
+       `;
+}
+
+function f2toB1() {
+  return `
+  <div id="b1" class="back-content">
+            <h4 class="bio">Bio</h4>
           <div class="tahun">
             <div class="tahun-detail">
               <div class="circle"></div>
@@ -193,30 +159,6 @@ function b1toF1() {
               Melanjutkan pendidikan kuliah di Universitas Binaniaga Indonesia
             </h4>
           </div>
-          </div>
-        </div>
-       `;
-}
-
-function f2toB1() {
-  return `
-  <div id="b1" class="back-content">
-            <h2>The Mogus</h2>
-            <div class="bg">
-              <img
-                src="./assets/LogoAnimation/logoblack3.svg"
-                class="aboutImg"
-                alt=""
-           
-              />
-            </div>
-            <h4>
-              Perkenalkan, dia adalah Mogus. Karakter ini terinspirasi dari
-              salah satu <i>game</i> terkenal, <i>Among Us</i>, yang di mana ada
-              sebuah plesetan pada namanya menjadi Amogus. Jika pada mode terang
-              Mogus akan berubah menjadi warna merah yang dinamakan Red Mogus.
-              Sedangkan pada mode gelap berubah menjadi White Mogus.
-            </h4>
           </div>`;
 }
 
